84cee16a205930a3c543f8357b6ef88abc550ead
Greedywebminer <168652146+Greedywebminer@users.noreply.github.com>
2024-09-28 14:27:30 -0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,18 +1,19 @@
-
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Start an HTTP server and serve battery and wifi status
 while true; do
   # Get battery status
   battery=$(termux-battery-status | jq '.')
 
   # Get Wi-Fi status
-  wifi=$(termux-wifi-info | jq '.')
+  wifi=$(termux-wifi-info 2>&1)
+  if [ $? -ne 0 ]; then
+    wifi="{\"error\": \"Failed to retrieve Wi-Fi info: $wifi\"}"
+  else
+    wifi=$(echo "$wifi" | jq '.')
+  fi
 
-  # Output in JSON format
-  echo "Content-type: application/json"
-  echo ""
-  echo "{\"battery\": $battery, \"wifi\": $wifi}"
+  # Save JSON data to a file
+  echo "{\"battery\": $battery, \"wifi\": $wifi}" > /data/data/com.termux/files/home/status.json
 
   sleep 60  # Wait for 1 minute
 done
